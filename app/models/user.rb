7aa85a7c0d8050c0162fba95fc65cc5524f7f041
Truong Van Lam <truong.van.lam@sun-asterisk.com>
2019-08-28 11:31:14 +0700

    Chapter 7: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   before_save{email.downcase!}
-  validates :name,  presence: true, length: Settings.maximum_length_name
-  validates :email, presence: true, length: Settings.maximum_length_email,
+  validates :name,  presence: true,
+            length: {maximum: Settings.maximum_length_name}
+  validates :email, presence: true,
+            length: {maximum: Settings.maximum_length_email},
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: Settings.minimum_length_password
+  validates :password, presence: true,
+            length: {minimum: Settings.minimum_length_password}
 end
